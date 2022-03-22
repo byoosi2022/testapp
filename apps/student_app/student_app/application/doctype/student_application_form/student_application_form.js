e73ac94d6958c1f3ab2a296987f2014ace839db5
byoosi2022 <byoosi2021@gmail.com>
2022-03-22 16:15:33 +0000

    save arezzo files

---
+++ apps/student_app/student_app/application/doctype/student_application_form/student_application_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, paul and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Student Application form', {
+	// refresh: function(frm) {
+
+	// }
+});
